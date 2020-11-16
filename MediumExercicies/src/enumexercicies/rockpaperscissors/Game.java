3340a5943c06245bbc72651d2d88300dd62ebed8
diogo1080 <wersade10@gmail.com>
2020-11-16 09:44:36 +0000

    correção

--- MediumExercicies/src/enumexercicies/rockpaperscissors/Game.java
+++ MediumExercicies/src/enumexercicies/rockpaperscissors/Game.java
@@ -3,25 +3,29 @@ package enumexercicies.rockpaperscissors;
 
 public class Game {
     private final int numRounds;
+    private Player p1;
+    private Player p2;
 
     public Game(int numRounds) {
         this.numRounds = numRounds;
     }
 
     public void start(Player player1, Player player2) {
-        roundController(player1, player2);
+        this.p1 = player1;
+        this.p2 = player2;
+        roundController();
     }
 
-    private void roundController(Player p1, Player p2) {
+    private void roundController() {
         for (int i = 0; i < numRounds; i++) {
             if (p1.getNumPoints() > numRounds / 2 || p2.getNumPoints() > numRounds / 2) {
                 break;
             }
-            round(p1, p2);
+            round();
         }
 
         while (p1.getNumPoints() == p2.getNumPoints()) {
-            round(p1, p2);
+            round();
         }
 
         if (p1.getNumPoints() > p2.getNumPoints()) {
@@ -32,7 +36,7 @@ public class Game {
 
     }
 
-    private void round(Player p1, Player p2) {
+    private void round() {
 
         HandStates player1Hand = p1.getHand();
         HandStates player2Hand = p2.getHand();
